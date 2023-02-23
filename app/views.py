bce767163b8a3de24e011eee9d9df14e5d0d7c1b
Sakhawy <50373064+sakhawy@users.noreply.github.com>
2023-02-23 22:13:09 +0200

    feat: add usd-to-egp view

--- app/views.py
+++ app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+def usd_to_egp(request):
+    context = {
+        'usd_to_egp': 30,
+    }
+    return render(request, 'app/usd-to-egp.html', context=context)
